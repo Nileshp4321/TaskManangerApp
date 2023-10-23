220a2bbfa1a428b4c8bc50f4e1908f75e164e97d
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2023-10-23 13:24:52 +0530

    Task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer } from "react-toastify"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Register from "./components/Register"
+import TaskForm from "./components/TaskForm"
+import TaskList from "./components/TaskList"
+import { Toaster } from "react-hot-toast"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <BrowserRouter>
+    
+    <Routes>
+      <Route path="/" element={ <Home/> }/>
+      <Route path="/login" element={ <Login/> }/>
+      <Route path="/register" element={ <Register/> } />
+      <Route path="/taskform" element={ <TaskForm/> } />
+      <Route path="/taskList" element={ <TaskList/> } />
+    </Routes>
+    <ToastContainer autoClose={1000} theme="dark" />
+    <ToastContainer autoClose={1000} theme="dark" />
+    {/* <Toaster /> */}
+    </BrowserRouter>
+   </>
+  )
 }
-
-export default App;
+export default App
